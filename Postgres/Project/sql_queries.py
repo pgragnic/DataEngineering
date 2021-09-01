4f54358386b41909c3b1c8d85adb5dcff5e85643
Philippe Gragnic <pgragnic@hotmail.com>
2021-09-01 09:36:02 +0200

    add create and drop queries

--- Postgres/Project/sql_queries.py
+++ Postgres/Project/sql_queries.py
@@ -1,27 +1,36 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+query = "DROP TABLE "
+songplay_table_drop = query + "songplays"
+user_table_drop = query + "users"
+song_table_drop = query + "songs"
+artist_table_drop = query + "artists"
+time_table_drop = query + "time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays \
+                        (songplay_id int, start_time timestamp, \
+                        user_id int, level varchar, song_id varchar, \
+                        artist_id varchar, session_id int, \
+                        location varchar, user_agent varchar)""")
 
-user_table_create = ("""
-""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, \
+                    first_name varchar, last_name varchar, gender varchar, \
+                    level varchar)""")
 
-song_table_create = ("""
-""")
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar, \
+                    title varchar, artist_id varchar, year int, \
+                    duration numeric)""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists \
+                    (artist_id varchar, name varchar, location varchar, \
+                    latitude numeric, longitude numeric)""")
 
-time_table_create = ("""
-""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time \
+                    (start_time timestamp, hour int, day int, \
+                    week int, month varchar, year int, weekday int)
+                    """)
 
 # INSERT RECORDS
 
