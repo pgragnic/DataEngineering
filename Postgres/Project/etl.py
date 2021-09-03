01110a9642648a6d9cd45ce472453215a679b65f
Philippe Gragnic <pgragnic@hotmail.com>
2021-09-03 11:30:49 +0200

    add docstrings to all functions in etl.py

--- Postgres/Project/etl.py
+++ Postgres/Project/etl.py
@@ -6,6 +6,19 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+
+    """
+    Description: This function is responsible to process song files in a directory,
+    and then ingesting data in songs and artists tables of the Sparkify database
+
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+
+    Returns:
+        None
+    """
+
     # open song file
     df = pd.read_json(filepath, lines=True)
 
@@ -22,6 +35,19 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+
+    """
+    Description: This function is responsible to process log files in a directory,
+    and then ingesting data in songplays, user and time tables of the Sparkify database
+
+    Arguments:
+        cur: the cursor object.
+        filepath: song data file path.
+
+    Returns:
+        None
+    """
+
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -66,6 +92,22 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+
+    """
+    Description: This function is responsible for listing the files in a directory,
+    and then executing the ingest process for each file according to the function
+    that performs the transformation to save it to the database.
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+        filepath: log data or song data file path.
+        func: function that transforms the data and inserts it into the database.
+
+    Returns:
+        None
+    """
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
