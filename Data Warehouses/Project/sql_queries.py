63a3a9b6f1689c36a810fc36570f93c5a234e37a
Philippe Gragnic <pgragnic@hotmail.com>
2021-09-15 13:19:50 +0200

    dwh: create and drop tables queries & scripts

--- Data Warehouses/Project/sql_queries.py
+++ Data Warehouses/Project/sql_queries.py
@@ -17,20 +17,16 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
-    CREATE TABLE IF NOT EXISTS
-    (
-        
-    )
-    """)
+#staging_events_table_create= ("""
+#    """)
 
-staging_songs_table_create = ("""
-""")
+#staging_songs_table_create = ("""
+#""")
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays 
         (
-            songplay_id SERIAL PRIMARY KEY,
+            songplay_id double precision DEFAULT nextval('songplays_seq') NOT NULL,
             start_time bigint NOT NULL,
             user_id int NOT NULL,
             level varchar NOT NULL,
@@ -116,7 +112,9 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+#create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+#drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
