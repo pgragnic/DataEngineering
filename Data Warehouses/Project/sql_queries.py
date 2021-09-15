86058f157f542c7c68207f207ee0c2d76ffaa61d
Philippe Gragnic <pgragnic@hotmail.com>
2021-09-15 14:12:10 +0200

    dwh: add staging tables in sql queries

--- Data Warehouses/Project/sql_queries.py
+++ Data Warehouses/Project/sql_queries.py
@@ -17,11 +17,45 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-#staging_events_table_create= ("""
-#    """)
+staging_events_table_create= ("""
+    CREATE TABLE IF NOT EXISTS staging_events
+        (
+            artist varchar,
+            auth varchar NOT NULL,
+            firstName varchar NOT NULL,
+            gender varchar NOT NULL,
+            itemInSession integer NOT NULL,
+            lastName varchar NOT NULL,
+            length numeric,
+            level varchar NOT NULL,
+            location varchar NOT NULL,
+            method varchar NOT NULL,
+            page varchar NOT NULL,
+            registration numeric NOT NULL,
+            sessionId integer NOT NULL,
+            song varchar,
+            status integer NOT NULL,
+            ts timestamp NOT NULL,
+            userAgent varchar NOT NULL,
+            userId integer NOT NULL
+        );
+""")
 
-#staging_songs_table_create = ("""
-#""")
+staging_songs_table_create = ("""
+    CREATE TABLE IF NOT EXISTS staging_songs
+        (
+            num_songs integer NOT NULL,
+            artist_id varchar NOT NULL,
+            artist_latitude numeric,
+            artist_longitude numeric,
+            artist_location varchar,
+            artist_name varchar NOT NULL,
+            song_id varchar NOT NULL,
+            title varchar NOT NULL,
+            duration numeric NOT NULL,
+            year integer NOT NULL
+        );
+""")
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays 
@@ -112,9 +146,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-#create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-#drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
