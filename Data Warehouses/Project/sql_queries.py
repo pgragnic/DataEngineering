e1842084876cf7a58c66c4d84d58f8686f47af0f
Philippe Gragnic <pgragnic@hotmail.com>
2021-09-16 09:52:48 +0200

    dwh: insertion in artists done

--- Data Warehouses/Project/sql_queries.py
+++ Data Warehouses/Project/sql_queries.py
@@ -164,6 +164,15 @@ song_table_insert = ("""
 """)
 
 artist_table_insert = ("""
+        INSERT INTO artists (artist_id, name, location, latitude, longitude)
+        SELECT
+            DISTINCT (artist_id) as artist_id,
+            artist_name as name,
+            artist_location as location,
+            artist_latitude as latitude,
+            artist_longitude as longitude 
+        FROM staging_songs
+        WHERE artist_id IS NOT NULL
 """)
 
 time_table_insert = ("""
@@ -175,4 +184,4 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 #insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-insert_table_queries = [song_table_insert]
+insert_table_queries = [artist_table_insert]
